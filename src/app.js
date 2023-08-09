33ada63dc6de48a86518181315447147c5b34b50
Dher82 <126974687+Dher82@users.noreply.github.com>
2023-08-09 22:59:03 +0000

    my excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,50 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".excuse").innerHTML = generateExcuse();
+  const p = document.getElementById("parrafo");
+
+  p.innerText = "Guess What Happened!";
+};
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWho = Math.floor(Math.random() * who.length);
+  console.log(randomWho);
+
+  console.log(who[randomWho]);
+  //acá termina el who//
+
+  let randomaction = Math.floor(Math.random() * action.length);
+  console.log(randomaction);
+
+  console.log(action[randomaction]);
+
+  //acá termina action//
+  let randomwhat = Math.floor(Math.random() * what.length);
+  console.log(randomwhat);
+
+  console.log(what[randomwhat]);
+  //acá termina what
+  let randomwhen = Math.floor(Math.random() * when.length);
+  console.log(randomwhen);
+
+  console.log(when[randomwhen]);
+  return (
+    who[randomWho] +
+    " " +
+    action[randomaction] +
+    " " +
+    what[randomwhat] +
+    " " +
+    when[randomwhen]
+  );
 };
